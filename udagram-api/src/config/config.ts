701f9a221fe2d189bc4005b0a79f947b102f0164
norhan <norhan.habila@gmail.com>
2022-05-31 20:06:31 +0200

    h

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -8,9 +8,9 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
 
-  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
